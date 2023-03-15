7a53c4e68628fd338dd61c46c2be49fb8d06d620
yhyawy <yhyawy1@gmail.com>
2023-03-15 20:49:23 +0200

    day 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import "./App.css";
+import { useState } from "react";
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-   const todos = [new Todo("Learn React"), new Todo("Learn TS")];
+   const [todos, setTodos] = useState<Todo[]>([]);
+   // const todos = [new Todo("Learn React"), new Todo("Learn TS")];
+
+   const addTodoHandler = (todoText: string) => {
+      const newTodo = new Todo(todoText);
+
+      setTodos((prevTodos) => {
+         return prevTodos.concat(newTodo);
+      });
+   };
+
+   const removeTodoHandler = (todoId: string) => {
+      setTodos((prev) => {
+         return prev.filter((todo) => todo.id !== todoId);
+      });
+   };
+
    return (
       <div>
-         <Todos items={todos} />
+         <NewTodo onAddTodo={addTodoHandler} />
+         <Todos items={todos} onRemoveTodo={removeTodoHandler} />
       </div>
    );
 }
